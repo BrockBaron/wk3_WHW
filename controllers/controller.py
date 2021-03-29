087b639dad1f8d14f36fee0cc8ff79311363d3b5
Nick Lekkas <nslekkas@gmail.com>
2021-03-29 08:57:03 +0100

    extensions

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,5 +1,5 @@
 from rps import app
-from flask import render_template
+from flask import render_template, request, redirect
 from models.player import Player
 from models.game import Game
 import random
@@ -9,11 +9,20 @@ import random
 def index():
     return render_template('index.html', title='Home')
 
-@app.route('/rps/<p1_choice>/<p2_choice>')
-def play(p1_choice, p2_choice):
+
+@app.route('/rps/pvp/<p1_choice>/<p2_choice>')
+def play_pvp(p1_choice, p2_choice):
     p1 = Player('Rodrigo', p1_choice)
     p2 = Player('Miguel', p2_choice)
     play_game = Game(p1, p2)
     winner =play_game.play_rps()
-    return render_template('pvp.html', winner=winner, p1=p1, p2=p2)
+    return render_template('pvp.html', winner=winner, p1=p1, p2=p2); return redirect('/rps')
 
+@app.route('/rps/pvc/<p1_choice>')
+def play_pvc(p1_choice):
+    choice = ['rock','paper','scissors']
+    p1 = Player('Rodrigo', p1_choice)
+    p2 = Player('HAL 9000', random.choice(choice))
+    play_game = Game(p1, p2)
+    winner =play_game.play_rps()
+    return render_template('pvc.html', winner=winner, p1=p1, p2=p2); return redirect('/rps')
